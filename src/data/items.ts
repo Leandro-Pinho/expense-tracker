3e0497cb667f70e2d184feac675d1d9ffb8fcf2d
Leandro-Pinho <leandroessencia@gmail.com>
2023-07-15 13:37:13 -0300

    create data type, add of the data items

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,4 +1,6 @@
-export const items = [
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
     {
         date: new Date(2023, 6, 15), category: 'food', title: 'McDonalds', value: 32.12
     },
